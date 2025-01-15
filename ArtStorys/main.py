8ba99a0b5eef89c23c135420a3892c4a807604aa
Simon Salamun <79207933+sim1sang1songs1@users.noreply.github.com>
2025-01-15 16:39:40 +0100

    Multiple Averages similarity Ansatz für word2vec

--- ArtStorys/main.py
+++ ArtStorys/main.py
@@ -60,30 +60,35 @@ def filesToStrings():
 
 def rank_art_stories_python_function(query):
 
+    # Dieser Ansatz vergleicht die Cosine Similarity jedes Tokens der Query, innerhalb der word2vec Models der einzelnen Artikel.
+    # Daraufhin wird die durchschnittliche Similarity der Tokens innerhalb jedes Artikels berechnet.
+    # Die Artikel werden dann nach der durchschnittlichen Similarity absteigend sortiert bzw. gerankt.
     if is_word2vec:
         documents = documentToWord2Vec()
         query_tokens = word_tokenize(query.lower())
 
         rankings = []
         for document in documents:
-
             similarities = []
-
-            for token in query_tokens:
-
-                if token in document["model"].wv:
-
-                    similarities.append(document["model"].wv.similarity(token, token))
+            for i in range(len(query_tokens)):
+                for j in range(i + 1, len(query_tokens)):
+                    token1 = query_tokens[i]
+                    token2 = query_tokens[j]
+                    if token1 in document["model"].wv and token2 in document["model"].wv:
+                        similarity = document["model"].wv.similarity(token1, token2)
+                        similarities.append(similarity)
+                    else:
+                        similarities.append(0)
 
             if similarities:
                 average_similarity = sum(similarities) / len(similarities)
-                print(average_similarity)
             else:
                 average_similarity = 0
             rankings.append((document["site"], average_similarity))
 
         rankings.sort(key=lambda x: x[1], reverse=True)
 
+        #TODO: Return the rankings to the frontend !!! Only for Word2Vec
         for rank in rankings:
             print(f"Document: {rank[0]}, Similarity: {rank[1]}")
 
