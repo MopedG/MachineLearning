ad019af43f92b043b44abc82bd81760c2c3a7ab3
bdav-dev <david.berezowski1@gmail.com>
2025-01-24 16:44:18 +0100

    Added stuff

--- WissensgraphQueryInterface/main.py
+++ WissensgraphQueryInterface/main.py
@@ -1,7 +1,26 @@
+"""
+Prüfungsaufgabe 3
+Programmieren Sie ein Wissensgraph Query Interface für ein Graph Neural Network der auf Moodle bereitgestellten
+Kunstshow-Ontologie als Streamlit-App für die Abfrage durch einen Kunstshow-Experten.
 
+a) App-Aufbau
+    1 User Query: Anfrage (=Prompt) in natürlicher Sprache.
+    2 CNF Expression: Ausgabe in konjunktiver Normalform.
+    3 Highlighted Subgraph: Visueller Fokus auf die relevanten Knoten und Kanten.
+    4 SPARQL Query: Abfrage des RDF-Graphen.
+    5 Missing Node Classification: vorhergesagte fehlende binäre Knotenklasse VIPStatus ja/nein.
+    6 Query Response: Ergebnis der Abfrage.
 
+b) Erweitern Sie die Streamlit-App um den Anwendungsfall der zielgruppenorientierten Ansprache
+von ähnlichen Kontakten und Person-Accounts (=Personen, die keiner Firma zugeordnet werden
+können) für Newsletter Marketing-Kampagnen.
+    - Beispiel-Anfrage: "Welche Kontakte können beim Versand meiner
+      Newsletter Marketing-Kampagne zu einer Zielgruppe zusammengefasst
+      werden?"
+    - Tipp: Beginnen Sie mit wenigen Kontakten und Person-Accounts.
+"""
 
+import spacy
 
-print("Hello world")
-
+nlp = spacy.load("")
 
