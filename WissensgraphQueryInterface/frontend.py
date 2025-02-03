0f2bfb2a9de5e23d2f77c4b16a37fdbb0735cfd4
bdav-dev <david.berezowski1@gmail.com>
2025-02-03 14:51:59 +0100

    OntologyQueryInterface implemented beginning & CNF generator

--- WissensgraphQueryInterface/frontend.py
+++ WissensgraphQueryInterface/frontend.py
@@ -1,62 +1,96 @@
 import streamlit as st
 
+import main
+
+def input_validation(user_input_x, user_input_z):
+    if user_input_x is None:
+        if user_input_z.strip() == "":
+            return "Es ist eine Eingabe erforderlich."
+
+    if user_input_z is None:
+        if user_input_x.strip() == "":
+            return "Es ist eine Eingabe erforderlich."
+
+    if user_input_z is not None and user_input_x is not None:
+        if user_input_z.strip() == "" and user_input_x.strip() == "":
+            return "Es ist eine Eingabe erforderlich."
+
+        if (not user_input_z.strip() == "") and (not user_input_x.strip() == ""):
+            return "Es darf maximal ein Eingabefeld mit Text gefüllt sein."
+
+    return None
+
+
 st.title("Abfrage der Kunstshow-Ontologie")
 st.subheader("Wissensgraph Query Interface")
 
-q = [
-    {
-        "vorlage": "Wie viele X hat Y gekauft?",
-        "query": [
-            "Wie viele",
-            {
-                "placeholder": "Gemälde"
-            },
-            "hat",
-            {
-                "placeholder": " Person"
-            },
-            "gekauft?"
-        ]
-    },
-    {
-        "vorlage": "Aus welchem Land kommt der Account X?",
-        "query": [
-            "Aus welchem Land kommt der Account",
-            { "placeholder": "Accountname" },
-            "?"
-        ]
-    }
-]
+x, y, z = st.columns(3)
 
-selection = st.selectbox(
-    label="Abfragevorlage",
-    options=[template["vorlage"] for template in q]
-)
+nodes_x, nodes_y, nodes_z = main.get_template_node_sets()
 
-if selection:
-    query = None
-    for template in q:
-        if template["vorlage"] == selection:
-            query = template
+selection_x = x.selectbox(label="x", options=list(nodes_x))
+selection_y = y.selectbox(label="y", options=list(nodes_y))
+selection_z = z.selectbox(label="z", options=list(nodes_z))
 
-    columns = st.columns(len(query["query"]), vertical_alignment="bottom", gap="small")
-    for i in range(len(query["query"])):
-        part = query["query"][i]
-        if isinstance(part, dict):
-            columns[i].text_input(label=part["placeholder"], label_visibility="hidden", key=part["placeholder"], placeholder=part["placeholder"])
+query_template = main.get_query_template(selection_x, selection_y, selection_z)
+
+if query_template is None:
+    st.error("Invalide Kombination")
+else:
+    st.divider()
+    input_x, input_y, input_z = st.columns(3, vertical_alignment="bottom")
+
+    node_x = query_template["template"]["x"]["node"]
+    user_input_x = None
+    if main.does_query_template_support_input(query_template, "x"):
+        user_input_x = input_x.text_input(label=node_x, placeholder=query_template["template"]["x"]["field"])
+    else:
+        input_x.text(node_x)
+
+    input_y.text(query_template["template"]["y"]["node"])
+
+    node_z = query_template["template"]["z"]["node"]
+    user_input_z = None
+    if main.does_query_template_support_input(query_template, "z"):
+        user_input_z = input_z.text_input(label=node_z, placeholder=query_template["template"]["z"]["field"])
+    else:
+        input_z.text(node_z)
+
+    query_examples = main.get_examples_of_query_template_queries(query_template)
+
+    if main.does_query_template_support_multiple_queries(query_template):
+        query_examples_string = '  \n'.join(map(lambda ex: f"`{ex}`", query_examples))
+        st.info(
+            f"""
+            *Diese Kombination erlaubt mehrere Abfragemöglichkeiten.*  
+            Zum Beispiel:  
+            {query_examples_string}  
+            **Lass das eine Eingabefeld leer, um den Wert des anderen abzufragen.**
+            """
+        )
+    else:
+        st.info(
+            f"""
+            Beispielabfrage:
+            `{query_template["queries"][0]["example"]}`
+            """
+        )
+
+    pressed = st.button("Abfragen")
+    if pressed:
+        error_text = input_validation(user_input_x, user_input_z)
+
+        if error_text:
+            st.error(error_text)
         else:
-            columns[i].text(part)
+            user_input, query = main.choose_query(query_template, user_input_x, user_input_z)
+            query_result_string = '  \n'.join(map(lambda res: f"`{res}`",main.query_ontology(query_template, query, user_input)))
 
-
-queryAufbau = "WELCHE X HAT Y "
-X = "X: bspw. PersonAccount, Contact, Artwork, Ticket, User, Account, Event"
-Y = "Y: bspw. Name, Title, TicketType, BillingState, EventName, AccountName"
-
-dict = {
-    "query1": "Aus welchem Land kommt der Account 'Wealth Management AG'?",
-    "query2": "Welches Artwork hat den Titel 'Golden Statue'?",
-    "query3": "Welche ArtworkMedium haben den artMediumTitle 'Photography'?",
-    "query4": "Welche Tickets haben den TicketType 'Premium Ticket'?",
-    "query5": "Welche PersonAccounts kommen aus dem BillingState 'USA'?",
-    "query6": "Welche Contacts haben eine mail unter '@test.com'?"
-}
+            st.success(
+            f"""
+            **Ergebnis der Abfrage**  
+            {query_result_string}    
+            **Konjunktive Normalform**  
+            `{main.build_cnf(query_template, query["input"], user_input)}`
+            """
+            )
--- WissensgraphQueryInterface/main.py
+++ WissensgraphQueryInterface/main.py
@@ -27,27 +27,15 @@ ERKENNTNISSE: NLP mit Spacy ist sehr unregelmäßig, d.h. es ist schwer, ein all
 - Wir haben uns jedoch dazu entschlossen, einige Beispiel Queries zu definieren, die wir dann in CNF umwandeln.
 """
 
-import ollama
+# import ollama
 from owlready2 import get_ontology, sync_reasoner, default_world
 from rdflib import Graph
 
-dict = {
-    "query1": "Aus welchem Land kommt der Account 'Wealth Management AG'?",
-    "query2": "Welches Artwork hat den Titel 'Golden Statue'?",
-    "query3": "Welche ArtworkMedium haben den artMediumTitle 'Photography'?",
-    "query4": "Welche Tickets haben den TicketType 'Premium Ticket'?",
-    "query5": "Welche PersonAccounts kommen aus dem BillingState 'USA'?",
-    "query6": "Welche Contacts haben eine mail unter '@test.com'?"
-}
-
-
 current_directory = os.path.dirname(os.path.abspath(__file__))
 ontology_file = os.path.join(current_directory, 'art_show_ontology.ttl')
-print(ontology_file)
 
 graph = Graph()
 graph.parse(ontology_file, format="ttl")
-
 
 res = graph.query(
     """
@@ -77,28 +65,161 @@ for row in res:
 # X: Person, Y: TicketFor, Z: bought
 # X: Contact, Y: Account, Z: employedBy
 
-option = {
-    "template": {
-        "x": "Contact",
-        "y": "employedBy",
-        "z": "Account"
+def query_ontology(query_template, query, user_input):
+    query_result = graph.query(
+        query["queryTemplate"].replace("$", user_input)
+    )
+
+    row_name = query_template["template"][query["output"]]["field"]
+
+    result = []
+    for row in query_result:
+        result.append(f"{row[row_name]}")
+
+    return result
+
+
+def build_cnf(query_template, query_input, user_input):
+    template = query_template["template"]
+
+    node_x = template["x"]["node"]
+    node_y = template["y"]["node"]
+    node_z = template["z"]["node"]
+
+    node_x_char = node_x[0]
+    node_z_char = node_z[0]
+
+    field = template[query_input]["field"]
+
+    if query_input == "x":
+        exists_node_char = node_x_char
+        searched_node_char = node_z_char
+    else:
+        exists_node_char = node_z_char
+        searched_node_char = node_x_char
+
+    return f"q = {searched_node_char}? * ∃{exists_node_char}: {node_x}({node_x_char}{'?' if node_x_char == searched_node_char else ''}) ∧ {node_z}({node_z_char}{'?' if node_z_char == searched_node_char else ''}) ∧ {node_y}({node_x_char}{'?' if node_x_char == searched_node_char else ''}, {node_z_char}{'?' if node_z_char == searched_node_char else ''}) ∧ {field}({exists_node_char}, '{user_input}')"
+
+
+query_templates = [
+    {
+        "template": {
+            "x": { "node": "Contact", "field": "contactFullName" },
+            "y": { "node": "employedBy" },
+            "z": { "node": "Account", "field": "accountName" }
+        },
+        "queries": [
+            {
+                "input": "x",
+                "output": "z",
+                "example": "'Henry Smith' employedBy ?",
+                "queryTemplate": """
+                SELECT ?accountName WHERE
+                {            
+                    ?account a :Account ;
+                             :accountName ?accountName .
+                    ?contact a :Contact ;
+                             :contactFullName ?contactFullName ;
+                             :employedBy ?account .            
+                    FILTER(?contactFullName = "$")
+                }
+                """
+            },
+            {
+                "input": "z",
+                "output": "x",
+                "example": "? employedBy 'Art Gallery Inc.'",
+                "queryTemplate": """
+                SELECT ?contactFullName WHERE
+                {            
+                    ?account a :Account ;
+                             :accountName ?accountName .
+                    ?contact a :Contact ;
+                             :contactFullName ?contactFullName ;
+                             :employedBy ?account .            
+                    FILTER(?accountName = "$")
+                }
+                """
+            }
+        ]
     },
-    "input": {
-        "template": "x",
-        "field": "contactFullName"
+    {
+        "template": {
+            "x": { "node": "Account", "field": "accountName" },
+            "y": { "node": "sold" },
+            "z": { "node": "Artwork" }
+        },
+        "queries": [
+            { # TODO: SPARQL Query erroneous
+                "input": "x",
+                "output": "z",
+                "example": "'Art Gallery Inc.' sold ?",
+                "queryTemplate": """
+                SELECT ?title WHERE
+                {            
+                    ?account a :Account ;
+                             :accountName ?accountName .
+                             
+                    ?artwork a :Artwork ;
+                             :title ?title ;
+                             
+                    ?artworkSale a :ArtworkSale ;
+                             :soldArtwork ?artwork ;
+                             :sellingAccount ?account .
+                            
+                    FILTER(?accountName = "$")
+                }
+                """
+            }
+        ]
     },
-    "queryTemplate": """
-    SELECT ?accountName WHERE
-    {            
-        ?account a :Account ;
-                 :accountName ?accountName .
-        ?contact a :Contact ;
-                 :contactFullName ?contactFullName ;
-                 :employedBy ?account .            
-        FILTER(?contactFullName = "$")
-    }
-    """
-}
+
+]
+
+
+def get_template_node_sets():
+    x = {query_template["template"]["x"]["node"] for query_template in query_templates}
+    y = {query_template["template"]["y"]["node"] for query_template in query_templates}
+    z = {query_template["template"]["z"]["node"] for query_template in query_templates}
+
+    return x, y, z
+
+def get_query_template(x, y, z):
+    for query_template in query_templates:
+        template = query_template["template"]
+        if template["x"]["node"] == x and template["y"]["node"] == y and template["z"]["node"] == z:
+            return query_template
+    return None
+
+def does_query_template_support_input(query_template, template):
+    for query in query_template["queries"]:
+        if query["input"] == template:
+            return True
+    return False
+
+def does_query_template_support_multiple_queries(query_template):
+    return len(query_template["queries"]) > 1
+
+def get_examples_of_query_template_queries(query_template):
+    return [query["example"] for query in query_template["queries"]]
+
+def _get_query(query_template, query_input):
+    for query in query_template["queries"]:
+        if query["input"] == query_input:
+            return query
+
+def choose_query(query_template, user_input_x, user_input_z):
+    if user_input_x is None or user_input_x.strip() == "":
+        return user_input_z, _get_query(query_template, "z")
+
+    if user_input_z is None or user_input_z.strip() == "":
+        return user_input_x, _get_query(query_template, "x")
+
+
+
+
+
+
 
 #"cnf": "q = A? * ∃C: Contact(C) ∧ Account(A?) ∧ employedBy(C, A?) ∧ contactFullName(C, '$')",
 # Kunstwerk
